d30c4bfde66568a20295f164cff4899a0e86c857
Alex Balaguer <balaguer.dev@gmail.com>
2022-09-23 13:35:24 +0200

    80%

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,29 +20,83 @@ function tabla(datos){
     for(let valor of datos){
      
         contenido.innerHTML +=`
-    
-            <tr>
-                <th scope="row">${valor.id}</th>
-                <td>${valor.name} ${valor.lastName}</td>
-                <td>${valor.email}</td>
-                <td>${valor.phoneNumber}</td>
-                <td>${valor.city}</td>
-                <td>
+            <tr data-bs-toggle="modal" data-bs-target="#userId${valor.id}" style="cursor:pointer">
+                    <th scope="row">${valor.id}</th>
+                    <td>${valor.name} ${valor.lastName}</td>
+                    <td>${valor.email}</td>
+                    <td>${valor.phoneNumber}</td>
+                    <td>${valor.city}</td>
+                    <td>
 
-                 
-                    <button type="submit" class="btn btn-outline-primary"><a href="./employee.php?id=${valor.id}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
-                        </svg></a>
-                    </button>
+                    
+                        <button type="submit" class="btn btn-outline-primary"><a href="./employee.php?id=${valor.id}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
+                            </svg></a>
+                        </button>
 
-                    <button type="button" class="btn btn-outline-danger" onclick="deleteRow(${valor.id})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path>
-                        </svg>
-                    </button>
-                </td>
-            </tr>`
+                        <button type="button" class="btn btn-outline-danger" onclick="deleteRow(${valor.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path>
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+                
+                
+                
+<!-- MODAL -->
+<!-- SHOW EMPLOYEE INFO  -->
+    <div class="modal fade" id="userId${valor.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <h3 class="text-primary mb-1 mt-2 text-center" >${valor.name} ${valor.lastName} - ${valor.age} </h3>
+            <h6 class="text-primary mb-4 mt-1 text-center" >id : 000${valor.id}</h6>
+                
+                <div class="modal-body">
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input name="txtEmail"class="form-control" type="text" placeholder="${valor.email}" readonly>
+                        </div>
+                        <div class="col">
+                            <input name="txtPhone"class="form-control" type="Number" placeholder="${valor.phoneNumber}" readonly>
+                        </div>
+                    </div>
+        
+                    <div class="row mb-3">
+                        <div class="col ">
+                            <input name="txtPhone"class="form-control" type="Number" placeholder="${valor.streetAddress}" readonly>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input name="txtPhone"class="form-control" type="Number" placeholder="${valor.city}" readonly>
+                        </div>
+                        <div class="col">
+                            <input name="txtPhone"class="form-control" type="Number" placeholder="${valor.state}" readonly>
+                        </div>
+                        <div class="col">
+                            <input name="txtPhone"class="form-control" type="Number" placeholder="${valor.postalCode}" readonly>
+                        </div>
+                    </div>
+
+                    <div class="row mb-2">
+                        <div class="col d-flex justify-content-around">
+                            <input type="submit"   value="Update" id="btnadd" data-bs-dismiss="modal" class="btn btn-secondary mb-2 mt-3 btn-block></input>
+                        </div>
+                        <div class="col d-flex justify-content-around">
+                            <input type="submit"   value="Delete" id="btnadd" data-bs-dismiss="modal" class="btn btn-danger mb-2 mt-3 btn-block"></input>  
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>`
+
+
+
     }
 }
 
--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -2,11 +2,12 @@
 
 
 /* AGREGAR EMPLEADO// EMPLEADO NUEVO */
-function addEmployees($newEmployee){
+function addEmployees($newEmployee)
+{
     $path = file_get_contents("../../resources/employees.json");
     $employees = json_decode($path, true);
-    
-    $newEmployee["id"] = end($employees)["id"]+1;          //función para que no se repitan los id, y autoasignarles un numero dependiendo del último
+
+    $newEmployee["id"] = end($employees)["id"] + 1;          //función para que no se repitan los id, y autoasignarles un numero dependiendo del último
     array_push($employees, $newEmployee);
     file_put_contents("../../resources/employees.json", json_encode($employees));
     echo json_encode($employees);
@@ -17,54 +18,51 @@ function addEmployees($newEmployee){
 
 
 /* EDITAR EMPLEADO */
- function editEmployee(array $updateEmployee){
-/*     $editRow = $_GET['editRow'];*/    
-    
+function editEmployee(array $updateEmployee)
+{
+    /*     $editRow = $_GET['editRow'];*/
+
     $employees = json_decode(file_get_contents("../../resources/employees.json"), true);
- 
+
     /* 
     if(!empty($employees)) */
-    for($i=0 ; $i< count($employees); $i++){ 
-        for($e=0 ; $e< count($employees[$i]); $e++){
-        if($employees[$e]["id"]     ==    $updateEmployee['id']){
-            
-            $employees[$e]["name"]            =      $updateEmployee['name'];
-            $employees[$e]["lastName"]        =      $updateEmployee['lastName'];
-            $employees[$e]["email"]           =      $updateEmployee['email'];
-            $employees[$e]["city"]            =      $updateEmployee['city'];
-            $employees[$e]["streetAddress"]   =      $updateEmployee['streetAddress'];
-            $employees[$e]["state"]           =      $updateEmployee['state'];
-            $employees[$e]["age"]             =      $updateEmployee['age'];
-            $employees[$e]["postalCode"]      =      $updateEmployee['postalCode'];
-            $employees[$e]["phoneNumber"]     =      $updateEmployee['phoneNumber'];
-            
-        
+    for ($i = 0; $i < count($employees); $i++) {
+        for ($e = 0; $e < count($employees[$i]); $e++) {
+            if ($employees[$e]["id"]     ==    $updateEmployee['id']) {
+
+                $employees[$e]["name"]            =      $updateEmployee['name'];
+                $employees[$e]["lastName"]        =      $updateEmployee['lastName'];
+                $employees[$e]["email"]           =      $updateEmployee['email'];
+                $employees[$e]["city"]            =      $updateEmployee['city'];
+                $employees[$e]["streetAddress"]   =      $updateEmployee['streetAddress'];
+                $employees[$e]["state"]           =      $updateEmployee['state'];
+                $employees[$e]["age"]             =      $updateEmployee['age'];
+                $employees[$e]["postalCode"]      =      $updateEmployee['postalCode'];
+                $employees[$e]["phoneNumber"]     =      $updateEmployee['phoneNumber'];
             }
         }
-        
-        
         $employees = json_encode($employees);
         file_put_contents("../../resources/employees.json", $employees);
-  
-    header('location: ../dashboard3.php'); 
+
+        header('location: ../dashboard3.php');
     }
 }
 
 
 
 
-/* DELETE EMPLEADO */ 
-function deleteEmployee($deleteId){
-    $path = json_decode(file_get_contents("../../resources/employees.json"),true);
-   
+/* DELETE EMPLEADO */
+function deleteEmployee($deleteId)
+{
+    $path = json_decode(file_get_contents("../../resources/employees.json"), true);
 
-    for ($e=0 ; $e< count($path) ; $e++){
 
-        if(strval($path[$e]["id"]) === $deleteId){
-        $employeesPos = array_search($path[$e],$path);
-        array_splice($path,$employeesPos,1);
+    for ($e = 0; $e < count($path); $e++) {
+
+        if (strval($path[$e]["id"]) === $deleteId) {
+            $employeesPos = array_search($path[$e], $path);
+            array_splice($path, $employeesPos, 1);
         }
-  
     }
     file_put_contents("../../resources/employees.json", json_encode($path));
     $arrayPath = (array)$path;
@@ -72,107 +70,88 @@ function deleteEmployee($deleteId){
 }
 
 
-function getEmployees($id){    
+function getEmployees($id)
+{
     $path = json_decode(file_get_contents("../resources/employees.json"), true);
 
-    for ($e=0 ; $e< count($path) ; $e++){
-//strval = string value = valor insertado lo convierte a un string
-        if($path[$e]["id"] == $id){
-            
-        $name = $path[$e]['name'];
-        $lastName = $path[$e]['lastName'];
-        $email = $path[$e]['email'];
-        $age = $path[$e]['age'];
-        $streetAddress = $path[$e]['streetAddress'];        
-        $city = $path[$e]['city'];
-        $state = $path[$e]['state'];
-        $postalCode = $path[$e]['postalCode'];
-        $phoneNumber= $path[$e]['phoneNumber'];
+    for ($e = 0; $e < count($path); $e++) {
+        //strval = string value = valor insertado lo convierte a un string
+        if ($path[$e]["id"] == $id) {
+
+            $name = $path[$e]['name'];
+            $lastName = $path[$e]['lastName'];
+            $email = $path[$e]['email'];
+            $age = $path[$e]['age'];
+            $streetAddress = $path[$e]['streetAddress'];
+            $city = $path[$e]['city'];
+            $state = $path[$e]['state'];
+            $postalCode = $path[$e]['postalCode'];
+            $phoneNumber = $path[$e]['phoneNumber'];
+
+            $form = <<< form
+        <form class="row g-3" method="POST" name="frmUpdate" action="./library/employeeController.php?action=edit&id=$id">
+ 
+            <div class="modal-dialog">
+                <h3 class="text-primary mb-3 mt-5 text-center" >Update employee</h3>
         
-        $form = <<< form
-        <form method="POST" name="frmUpdate" action="./library/employeeController.php?action=edit&id=$id">
-    
+                
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="ageUser" class="form-label">Age:</label>
+                            <input type="number" class="form-control" name="txtAge" value="$age" id="ageUser">
+                        </div>
+                        <div class="col">
+                            <label for="nameUser" class="form-label">Name:</label>
+                            <input type="text" class="form-control" name="txtName" value="$name" id="nameUser">
+                        </div>
+                        <div class="col">
+                            <label for="lastNameUser" class="form-label">Last name:</label>
+                            <input type="text" class="form-control" name="txtLastName" value="$lastName" id="lastNameUser">
+                        </div>
+                    </div>
 
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtName" value="$name" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
+                    <div class="row mb-3">
+                        <div class="col ">
+                            <label for="emailUser" class="form-label">Email:</label>
+                            <input type="email" class="form-control"  name="txtEmail" value="$email" id="emailUser">
+                        </div>
+                        <div class="col">
+                        <label for="phoneUser" class="form-label">Phone:</label>
+                        <input type="number" class="form-control" name="txtPhone" value="$phoneNumber" id="phoneUser">
+                        </div>
+                    </div>
+        
+                    <div class="row mb-3">
+                        <div class="col ">
+                            <label for="streetAddUser" class="form-label">Street address:</label>
+                            <input type="text" class="form-control" name="txtStreet" value="$streetAddress" id="streetAddUser">
+                        </div>
+                    </div>
 
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtLastName" value="$lastName" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
 
-            <div class="input-group mb-3">
-                <input type="text" class="form-control"  name="txtEmail" value="$email" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="cityUser" class="form-label">City:</label>
+                            <input type="text" class="form-control" name="txtCity" value="$city" id="cityUser">
+                        </div>
+                        <div class="col mb-3">
+                            <label for="stateUser" class="form-label">State</label>
+                            <input type="text" class="form-control" name="txtState" value="$state" id="stateUser">
+                        </div>
+                        <div class="col">
+                            <label for="postalCodeUser" class="form-label">Postal Code:</label>
+                            <input type="number" class="form-control" name="txtPostalC" value="$postalCode" id="postalCodeUser">
+                        </div>
+                    </div>
+                        
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    </div>
+                </div>
             </div>
-
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtAge" value="$age" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtStreet" value="$streetAddress" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtCity" value="$city" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtState" value="$state" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-            
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtPostalC" value="$postalCode" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="txtPhone" value="$phoneNumber" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            </div>
-
-                <input type="submit" value="Update" name="btnUpdate"> 
-
         </form>
 
-           /*  <form method="POST" name="frmUpdate" action="./library/employeeController.php?action=edit&id=$id">
-                <label> First Name:
-                    <input type="text" name="txtName" value="$name"> 
-                </label>
-
-                <label> Last Name:
-                    <input type="text" name="txtLastName" placeholder="$"> 
-                </label>
-
-                <label> Email:
-                    <input type="text" name="txtEmail" value="$email"> 
-                </label>
-
-                <label> Age:
-                    <input type="text" name="txtAge" value="$age"> 
-                </label>
-
-                <label> Street Adress:
-                    <input type="text" name="txtStreet" value="$streetAddress"> 
-                </label>
-
-                <label> City:
-                    <input type="text" name="txtCity" value="$city"> 
-                </label>
-
-                <label> State:
-                    <input type="text" name="txtState" value="$state"> 
-                </label>
-
-                <label> Postal Code:
-                    <input type="text" name="txtPostalC" value="$postalCode"> 
-                </label>
-
-                <label> Phone Number:
-                    <input type="text" name="txtPhone" value="$phoneNumber"> 
-                </label>
-
-                <input type="submit" value="Update" name="btnUpdate"> 
-            </form> */
         form;
-       
         }
     }
     echo $form;
@@ -184,17 +163,16 @@ function getEmployees($id){
 }  */
 
 
-function listEmployees(){    
+function listEmployees()
+{
     $path = json_decode(file_get_contents("../../resources/employees.json"), true);
     echo json_encode($path);
-
-    
 }
 
 
- function removeAvatar($id)
+function removeAvatar($id)
 {
-// TODO implement it
+    // TODO implement it
 }  
 
 
@@ -210,6 +188,3 @@ function listEmployees(){
 
 }
 */
-
-
-?>
