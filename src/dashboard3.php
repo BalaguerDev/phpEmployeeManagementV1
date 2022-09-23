a072e31e63d3b6dc6b59d19e9acc3eb6cbe465fd
Jackson Zhou <jianchenfutbol@gmail.com>
2022-09-23 14:27:48 +0200

    navbar

--- src/dashboard3.php
+++ src/dashboard3.php
@@ -22,7 +22,7 @@
                         <th scope="col">Id</th>
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
-                        <th scope="col">Phone Number</th>
+                        <th scope="col">Phone Num ber</th>
                         <th scope="col">City</th>
                         <th scope="col"><button type="button"  class="btn btn-success pull-right" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa fa-plus"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16"><path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/><path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/><th scope="col"><div class="d-flex justify-content-evently"></svg></button></th>
                     </tr>
@@ -42,7 +42,6 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <h3 class="text-primary mb-3 mt-2 text-center" id="exampleModalLabel">New Employee</h3>
-
                 <div class="modal-body">
                     <form method="post" name="frmAdd" class="row" id="addForm">
                         <div class="row">
