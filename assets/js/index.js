76de26af731ddd8ac3ab8b0edf23fb411b9eed63
Jackson Zhou <jianchenfutbol@gmail.com>
2022-09-23 16:57:04 +0200

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -138,4 +138,24 @@ function deleteRow(id){
 
 }
 
+//SEARCH 
+
+    function myFunction() {
+      var input, filter, table, tr, th, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+      for (i = 0; i < tr.length; i++) {
+        th = tr[i].getElementsByTagName("th")["1"];
+        if (th) {
+          txtValue = th.textContent || th.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+      }
+    }
 
